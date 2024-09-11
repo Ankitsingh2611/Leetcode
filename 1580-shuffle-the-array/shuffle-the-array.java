ef698ce66ba1635e7e18f39eb866f52bc44133af
Ankit Kumar Singh <165676021+Ankitsingh2611@users.noreply.github.com>
2024-09-11 21:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (16.25%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,10 +1,17 @@
 class Solution {
   public int[] shuffle(int[] nums, int n) {
     int[] ans = new int[2 * n];
-    for (int i = 0; i < n; ++i) {
-      ans[i * 2] = nums[i];
-      ans[i * 2 + 1] = nums[i + n];
+    
+    int left = 0;   // Pointer 1: points to the first element of the first half
+    int right = n;  // Pointer 2: points to the first element of the second half
+    
+    for (int i = 0; i < 2 * n; i += 2) {
+      ans[i] = nums[left];      // Place element from the first half
+      ans[i + 1] = nums[right]; // Place element from the second half
+      left++;  // Move pointer 1 to the next element in the first half
+      right++; // Move pointer 2 to the next element in the second half
     }
+    
     return ans;
   }
 }
