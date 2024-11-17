db22b6c7bce2840d3bcac77022df007e5e0e17be
Ankit Kumar Singh <165676021+Ankitsingh2611@users.noreply.github.com>
2024-11-17 18:57:38 +0530

    Time: 1 ms (36.41%) | Memory: 45 MB (84.73%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,10 +1,29 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        int ans = n;
-        for (int i = 0; i < n; ++i) {
-            ans ^= (i ^ nums[i]);
+       int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (nums[i] < nums.length && nums[i] != nums[correct]) {
+                swap(nums, i , correct);
+            } else {
+                i++;
+            }
         }
-        return ans;
+
+        // search for first missing number
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index) {
+                return index;
+            }
+        }
+
+        // case 2
+        return nums.length;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
     }
 }
