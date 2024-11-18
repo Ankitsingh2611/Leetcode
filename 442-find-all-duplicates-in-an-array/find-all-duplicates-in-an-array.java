ec5fea3b3c69c5bfff138270a2ccff947efeb2ac
Ankit Kumar Singh <165676021+Ankitsingh2611@users.noreply.github.com>
2024-11-18 17:46:57 +0530

    Time: 6 ms (74.80%) | Memory: 54.1 MB (80.82%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -1,24 +1,25 @@
-
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
-        int n = nums.length;
-        for (int i = 0; i < n; ++i) {
-            while (nums[i] != nums[nums[i] - 1]) {
-                swap(nums, i, nums[i] - 1);
+        int i =0;
+        while(i< nums.length){
+            int correct = nums[i] -1;
+            if(nums[i]!= nums[correct]){
+                swap(nums, i, correct);
+            } else {
+                i++;
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] != i + 1) {
-                ans.add(nums[i]);
+        List<Integer> ans = new ArrayList<Integer>();
+        for(int index = 0; index<nums.length; index++){
+            if(nums[index] != index+1){
+                ans.add(nums[index]);
             }
         }
         return ans;
     }
-
-    void swap(int[] nums, int i, int j) {
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
+     static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
     }
 }
